96d98710f1ea66b1aaec9609e2961d905a088508
Rohith Kappala <sa19agw>
2026-06-01 07:24:36 +0100

    updated frontend

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/event_management/',
+  base: '/Event_Management/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
